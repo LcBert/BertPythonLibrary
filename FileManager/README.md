c9854554b359125fbd5bd77db417a58e6d8ae7b0
Luca Bertaggia <luca.bertaggia82@gmail.com>
2023-05-28 22:37:51 +0200

    Some changes

--- FileManager/README.md
+++
@@ -1,6 +0,0 @@
-### FOR UPDATE ###
--Update the file in "FileManager" dir.
--Eliminate the file in "build/lib/FileManager".
--Go in the root dir and open cmd
--Activate the venv with "venv\Scripts\activate"
--Compile library with "python setup.py bdist_wheel"
--- FileManager/setup.py
+++ FileManager/setup.py
@@ -1,13 +1,11 @@
 from setuptools import find_packages, setup
+
 setup(
     name='FileManager',
-    packages=find_packages(include=['FileManager']),
-    version='0.1.0',
-    description='A python library for fast file management',
+    packages=find_packages(include=["FileManager"]),
+    version='0.0.1',
+    description='',
     author='Me',
     license='MIT',
     install_requires=[],
-    setup_requires=['pytest-runner'],
-    tests_require=['pytest==4.4.1'],
-    test_suite='tests',
 )
